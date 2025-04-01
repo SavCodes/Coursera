0b9d2566c384552704bc93aa2dcb13b421dbe4dd
SavCodes <91900522+SavCodes@users.noreply.github.com>
2025-04-01 14:50:51 -0400

    Create PrintFavoriteGames.cs

---
+++ C# and Unity Specialization/Course 1: Introduction to C# Programming and Unity/Exercise2/PrintFavoriteGames.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    void Start()
+    {
+        // Prints Three Video Game Titles
+        Debug.Log("Leauge, Overwatch, Halo");
+    }
+}
