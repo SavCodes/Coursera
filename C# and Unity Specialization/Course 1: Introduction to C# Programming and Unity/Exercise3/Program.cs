9a81fe8914e5818740141e3a17c4c328414a4e9a
SavCodes <91900522+SavCodes@users.noreply.github.com>
2025-04-01 14:52:07 -0400

    Create Program.cs

---
+++ C# and Unity Specialization/Course 1: Introduction to C# Programming and Unity/Exercise3/Program.cs
@@ -0,0 +1,15 @@
+namespace Exercise3
+{
+    internal class Program
+    {
+        /// <summary>
+        /// A program that outputs the developers age
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int age = 9001;
+            Console.WriteLine("Developers Age: ", age);
+        }
+    }
+}
