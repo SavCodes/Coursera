75f8fad49fc49dd312892500fede54e84ca61c6e
SavCodes <91900522+SavCodes@users.noreply.github.com>
2025-04-03 18:55:29 -0400

    Update Program.cs

--- C# and Unity Specialization/Course 4: Intermediate Object-Oriented Programming for Unity Games/Exercise1/Program.cs
+++ C# and Unity Specialization/Course 4: Intermediate Object-Oriented Programming for Unity Games/Exercise1/Program.cs
@@ -1,1 +1,34 @@
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input Validation
+            {
+                try
+                {
+                    // Get user score input
+                    Console.Write("Enter a score: ");
+                    int integerScore = int.Parse(Console.ReadLine());
+                }
+
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Input must be a number");
+                }
+
+                catch (OverflowException oe)
+                {
+                    Console.WriteLine(" 2^-31 < Your Input < 2^31");
+                }
+
+                finally
+                {
+                    Console.WriteLine("Input validation complete");
+                }
+            }
+        }
+    }
+}
 
