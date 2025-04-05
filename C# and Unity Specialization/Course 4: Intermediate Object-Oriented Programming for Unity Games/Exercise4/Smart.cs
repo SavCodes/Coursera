64ec52739d55f1afc369d346d1d4caedcbbfb7ab
SavCodes <91900522+SavCodes@users.noreply.github.com>
2025-04-04 21:11:43 -0400

    Create Smart.cs

---
+++ C# and Unity Specialization/Course 4: Intermediate Object-Oriented Programming for Unity Games/Exercise4/Smart.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Smart : Kid
+    {
+        public override void PrintMessage()
+        {
+            Console.WriteLine("This is a smart kid class message");
+        }
+    }
+}
