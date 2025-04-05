85450dd78609291741698f492e8a3a3f699f55c7
SavCodes <91900522+SavCodes@users.noreply.github.com>
2025-04-04 21:11:09 -0400

    Create Kid.cs

---
+++ C# and Unity Specialization/Course 4: Intermediate Object-Oriented Programming for Unity Games/Exercise4/Kid.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Kid
+    {
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the messge "This is a kid class message"
+        /// </summary>
+        virtual public void PrintMessage()
+        {
+            Console.WriteLine("This is a kid class message");
+        }
+        #endregion
+
+    }
+}
